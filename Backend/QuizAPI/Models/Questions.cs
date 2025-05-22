efd6dfa0eb972a709c64ac07c67a8eb342b18bd3
Leywin23 <dominikmikulski23@gmail.com>
2025-05-22 02:23:49 +0200

    change the size of database tables

--- Backend/QuizAPI/Models/Questions.cs
+++ Backend/QuizAPI/Models/Questions.cs
@@ -9,17 +9,17 @@ namespace QuizAPI.Models
         public int QnId { get; set; }
         [Column(TypeName= "nvarchar(250)")]
         public string QnInWords { get; set; }
-        [Column(TypeName ="nvarchar(50)")]
+        [Column(TypeName ="nvarchar(500)")]
         public string? qImage { get; set; }
-        [Column(TypeName = "nvarchar(50)")]
+        [Column(TypeName = "nvarchar(500)")]
         public string Option1 {  get; set; }
-        [Column(TypeName = "nvarchar(50)")]
+        [Column(TypeName = "nvarchar(500)")]
         public string Option2 {  get; set; }
-        [Column(TypeName = "nvarchar(50)")]
+        [Column(TypeName = "nvarchar(500)")]
         public string Option3 {  get; set; }
-        [Column(TypeName = "nvarchar(50)")]
+        [Column(TypeName = "nvarchar(500)")]
         public string Option4 {  get; set; }
-        [Column(TypeName = "nvarchar(50)")]
+        [Column(TypeName = "nvarchar(500)")]
         public string Answer { get; set; }
     }
 }
