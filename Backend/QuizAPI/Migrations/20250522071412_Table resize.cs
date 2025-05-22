1deaf0e9eca3de1940cb0e4bd405de0c4b6a3c96
Leywin23 <dominikmikulski23@gmail.com>
2025-05-22 09:16:18 +0200

    Migration

---
+++ Backend/QuizAPI/Migrations/20250522071412_Table resize.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tableresize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "qImage",
+                table: "Questions",
+                type: "nvarchar(500)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option4",
+                table: "Questions",
+                type: "nvarchar(500)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option3",
+                table: "Questions",
+                type: "nvarchar(500)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option2",
+                table: "Questions",
+                type: "nvarchar(500)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option1",
+                table: "Questions",
+                type: "nvarchar(500)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer",
+                table: "Questions",
+                type: "nvarchar(500)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "qImage",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option4",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option3",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option2",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Option1",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Answer",
+                table: "Questions",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)");
+        }
+    }
+}
